e9bf7adcfb225ac835706449ce977218ba6ff21a
Yashwanth Gowda M S <yashwanthgowdams474@gmail.com>
2021-12-01 15:23:30 +0530

    second commit

--- JavaGitApp/src/main/java/com/lti/demos/ClientApp.java
+++ JavaGitApp/src/main/java/com/lti/demos/ClientApp.java
@@ -2,6 +2,8 @@ package com.lti.demos;
 
 public class ClientApp {
 public static void main(String[] args) {
-	Product p1 =new Product 
+	Product p1 =new Product(101,"mobile");
+	System.out.println(p1);
+	System.out.println("hello");
 }
 }
--- JavaGitApp/src/main/java/com/lti/demos/Product.java
+++ JavaGitApp/src/main/java/com/lti/demos/Product.java
@@ -2,4 +2,8 @@ package com.lti.demos;
 
 public class Product {
 
+	public Product(int i, String string) {
+		// TODO Auto-generated constructor stub
+	}
+
 }
